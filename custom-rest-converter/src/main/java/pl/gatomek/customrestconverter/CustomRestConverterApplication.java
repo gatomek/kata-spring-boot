c11201fb2a7dede873d4f65a71bc83226aa65328
Gatomek <tomasz.galinski@gmail.com>
2025-05-10 12:36:24 +0200

    customer rest converters - project added

---
+++ custom-rest-converter/src/main/java/pl/gatomek/customrestconverter/CustomRestConverterApplication.java
@@ -0,0 +1,13 @@
+package pl.gatomek.customrestconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRestConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomRestConverterApplication.class, args);
+    }
+
+}
